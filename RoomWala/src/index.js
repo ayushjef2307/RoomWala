d4f1f9f87afd30df8cc09d48710e968ce3056a14
ayushjef2307 <92541201+ayushjef2307@users.noreply.github.com>
2022-07-06 22:47:42 +0530

    Add files via upload

---
+++ RoomWala/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from 'web3uikit';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider appId="xZJ504XKMJ1h0RKMqv5PZaTesyv5gDyuOxUEPOhy" serverUrl="https://b4u4smnkimhh.usemoralis.com:2053/server">
+      <NotificationProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      </NotificationProvider>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
